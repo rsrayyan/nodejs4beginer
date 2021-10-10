1ec6856d77cd7c5369d1236b9e05c4dc2a7d15c1
rayyan <rayyanshaikh54321@gmail.com>
2021-10-11 00:07:06 +0530

    2 hours completed of node js and express

---
+++ Node js/http modules/http_module.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url === ''){
+        res.write("welcome to home page")
+    }
+    if(req.url === '/about'){
+        res.end('here is yours short history')
+    }
+    res.write("welcome to my home page");
+    res.end()
+});
+
+server.listen(5000,()=>{
+    console.log("server is started");
+})
+
+//1:44 minutes
